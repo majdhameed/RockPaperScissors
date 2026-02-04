8929a45752c36b8daa777666ee05088089b08e35
Majd Hameed <majdhameed837@gmail.com>
2026-02-04 07:52:08 -0800

    added ui for rock paper scissors

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function getComputerChoice(){
+    //Randomly generate number between 1 and 99
     let randomInt = Math.floor(Math.random()*99+1)
     if (randomInt <= 33) {
         return 0
@@ -9,21 +10,23 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(){
-    let response = prompt("Enter rock, paper, or scissors")
+// function getHumanChoice(){
+//     let response = prompt("Enter rock, paper, or scissors")
 
-    if (response.toLowerCase() === "rock"){
-        return 0
-    }else if (response.toLowerCase() === "paper"){
-        return 1
-    }else if (response.toLowerCase() === "scissors"){
-        return 2
-    }
-}
+//     if (response.toLowerCase() === "rock"){
+//         return 0
+//     }else if (response.toLowerCase() === "paper"){
+//         return 1
+//     }else if (response.toLowerCase() === "scissors"){
+//         return 2
+//     }
+// }
 
-function round(humanChoice, computerChoice){
+function playRound(humanChoice, computerChoice){
+    console.log(humanChoice, computerChoice)
    if(humanChoice === computerChoice){
     return 0;
+    //RPS is a cyclic game so we can test it via (a+1)%3 = b
    }else if((humanChoice + 1)%3 != computerChoice){
     return 1;
    }else{
@@ -32,62 +35,69 @@ function round(humanChoice, computerChoice){
 }
 
 
+function game(playerChoice){
+    let roundsPlayed = 0;
+
+    let result;
+    let message;
+    computerChoice = getComputerChoice();
+
+    result = playRound(playerChoice, computerChoice)
+
+    if (result === 0){
+        message = `Round was a tie!
+        You both chose ${translation[playerChoice]}.`
+    }else if (result === 1){
+        humanScore += 1;
+        message = `You won the round!
+        ${translation[playerChoice]} beats ${translation[computerChoice]}`
+    }else{
+        computerScore += 1;
+        message = `Computer won the round!
+        ${translation[computerChoice]} beats ${translation[playerChoice]}`
+    }
+
+    event.textContent = message
+    playerScoreTracker.textContent = `Your score: ${humanScore}`
+    computerScoreTracker.textContent = `Computer score: ${computerScore}`
+    
+    console.log(message)
+
+    if (humanScore === pointsToWin){
+        alert("You won the game!")
+    }
+
+    if (computerScore === pointsToWin){
+        alert("Computer Won the game!")
+    }
+
+    
+}
+
+const buttons = document.querySelectorAll("button");
+console.log(buttons)
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    game(parseInt(button.getAttribute("data-move")))
+  });
+});
+
+
+event = document.querySelector(".event")
+playerScoreTracker = document.querySelector(".playerScore");
+console.log(playerScoreTracker)
+computerScoreTracker = document.querySelector(".computerScore")
+console.log(computerScoreTracker)
+
 humanScore = 0
 computerScore = 0
+pointsToWin=5;
 
 const translation = {
     0: "rock",
     1: "paper",
     2: "scissors"
 }
-
-function game(rounds){
-    let roundsPlayed = 0;
-
-    let result;
-    let message;
-    let threshold = Math.ceil(rounds/2)
-    while (roundsPlayed < rounds){
-        computerChoice = getComputerChoice()
-        playerChoice = getHumanChoice()
-
-        result = round(playerChoice, computerChoice)
-
-        if (result === 0){
-            message = `Round was a tie!
-            You both chose ${translation[playerChoice]}.
-            Current scores:
-            Your score: ${humanScore}
-            Computer Score: ${computerScore}`
-        }else if (result === 1){
-            humanScore += 1;
-            message = `You won the round!
-            ${translation[playerChoice]} beats ${translation[computerChoice]}
-            Current scores:
-            Your score: ${humanScore}
-            Computer Score: ${computerScore}`
-            roundsPlayed+=1
-        }else{
-            computerScore += 1;
-            message = `Computer won the round!
-            ${translation[computerChoice]} beats ${translation[playerChoice]}
-            Current scores:
-            Your score: ${humanScore}
-            Computer Score: ${computerScore}`
-            roundsPlayed +=1
-        }
-        
-        console.log(message)
-        if (humanScore === threshold){
-            console.log("You have won the game!")
-            break
-        }else if (computerScore === threshold){
-            console.log("Computer has won the game!")
-            break
-        }
-
-    }
-}
-
-
-game(5)
